b34c0bb77912e064651555eff450f1a5868c0b4d
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-06-08 21:55:03 -0400

    implement a route handler that inserts an employee based on query string parameters

--- sql-tut.go
+++ sql-tut.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -40,9 +41,32 @@ func getCurrentDb(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Current database is :: %s", db)
 }
 
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	vals := r.URL.Query()
+	name, ok := vals["name"]
+	if ok {
+		log.Print("Going to insert record in database for name: ", name[0])
+		stmt, err := db.Prepare("INSERT employee SET name = ?")
+		if err != nil {
+			log.Print("error preparing query :: ", err)
+			return
+		}
+		result, err := stmt.Exec(name[0])
+		if err != nil {
+			log.Print("error executing query :: ", err)
+			return
+		}
+		id, err := result.LastInsertId()
+		fmt.Fprint(w, "Last Inserted Record Id is :: %s", strconv.FormatInt(id, 10))
+	} else {
+		fmt.Fprint(w, "Error occurred while creating record in database for name :: %s", name[0])
+	}
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", getCurrentDb).Methods("GET")
+	router.HandleFunc("/employee/create", createEmployee).Methods("POST")
 	defer db.Close()
 	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, router)
 	if err != nil {
